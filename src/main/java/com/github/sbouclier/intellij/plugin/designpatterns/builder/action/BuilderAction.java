5a632ad4d04e68d19286b6a5b2cdeccfa49937a7
Stéphane Bouclier <sbouclier@users.noreply.github.com>
2018-10-26 21:39:47 +0200

    add builder action

---
+++ src/main/java/com/github/sbouclier/intellij/plugin/designpatterns/builder/action/BuilderAction.java
@@ -0,0 +1,12 @@
+package com.github.sbouclier.intellij.plugin.designpatterns.builder.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class BuilderAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
