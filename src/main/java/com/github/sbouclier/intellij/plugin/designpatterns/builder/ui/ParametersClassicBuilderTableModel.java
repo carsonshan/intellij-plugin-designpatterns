234dc848416f3e94d313dec1d5d20b46e9aee42d
Stéphane Bouclier <sbouclier@users.noreply.github.com>
2018-11-03 22:13:08 +0100

    split table model to classic and interface builder table model

--- src/main/java/com/github/sbouclier/intellij/plugin/designpatterns/builder/ui/ParametersClassicBuilderTableModel.java
+++ src/main/java/com/github/sbouclier/intellij/plugin/designpatterns/builder/ui/ParametersClassicBuilderTableModel.java
@@ -2,36 +2,18 @@ package com.github.sbouclier.intellij.plugin.designpatterns.builder.ui;
 
 import com.github.sbouclier.intellij.plugin.designpatterns.builder.model.BuilderParameter;
 
-import javax.swing.table.AbstractTableModel;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * Table model for parameters to Builder dialog
+ * Classic builder table model for parameters to Builder dialog
  */
-public class ParametersTableModel extends AbstractTableModel {
+public class ParametersClassicBuilderTableModel extends AbstractBuilderTableModel {
 
     private static final int COLUMN_PARAM_NAME_INDEX = 0;
     private static final int COLUMN_SETTER_NAME_INDEX = 1;
-    private static final int COLUMN_MANDATORY_INDEX = 2;
-    private static final int COLUMN_CONSTRUCTOR_INDEX = 3;
-
-    private final String[] headers = {"Parameter name", "Setter name", "Mandatory", "Constructor"};
-    private List<BuilderParameter> parameters = new ArrayList<>();
+    private static final int COLUMN_CONSTRUCTOR_INDEX = 2;
 
     @Override
-    public int getRowCount() {
-        return parameters.size();
-    }
-
-    @Override
-    public int getColumnCount() {
-        return headers.length;
-    }
-
-    @Override
-    public String getColumnName(int columnIndex) {
-        return headers[columnIndex];
+    String[] getHeaders() {
+        return new String[]{"Parameter name", "Setter name", "Constructor"};
     }
 
     @Override
@@ -41,8 +23,6 @@ public class ParametersTableModel extends AbstractTableModel {
                 return String.class;
             case COLUMN_SETTER_NAME_INDEX:
                 return String.class;
-            case COLUMN_MANDATORY_INDEX:
-                return Boolean.class;
             case COLUMN_CONSTRUCTOR_INDEX:
                 return Boolean.class;
             default:
@@ -57,8 +37,6 @@ public class ParametersTableModel extends AbstractTableModel {
                 return false;
             case COLUMN_SETTER_NAME_INDEX:
                 return false;
-            case COLUMN_MANDATORY_INDEX:
-                return true;
             case COLUMN_CONSTRUCTOR_INDEX:
                 return true;
             default:
@@ -73,8 +51,6 @@ public class ParametersTableModel extends AbstractTableModel {
                 return parameters.get(rowIndex).getParameterName();
             case COLUMN_SETTER_NAME_INDEX:
                 return parameters.get(rowIndex).getSetterName();
-            case COLUMN_MANDATORY_INDEX:
-                return parameters.get(rowIndex).isMandatory();
             case COLUMN_CONSTRUCTOR_INDEX:
                 return parameters.get(rowIndex).isConstructor();
             default:
@@ -85,26 +61,9 @@ public class ParametersTableModel extends AbstractTableModel {
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
         BuilderParameter param = parameters.get(rowIndex);
-        if (columnIndex == COLUMN_MANDATORY_INDEX) {
-            param.setMandatory(!param.isMandatory());
-            if (!param.isMandatory() && param.isConstructor()) {
-                param.setConstructor(false);
-                fireTableRowsUpdated(rowIndex, COLUMN_CONSTRUCTOR_INDEX);
-            }
+        if (columnIndex == COLUMN_CONSTRUCTOR_INDEX) {
+            param.setConstructor(!param.isConstructor());
             fireTableCellUpdated(rowIndex, columnIndex);
-        } else if (columnIndex == COLUMN_CONSTRUCTOR_INDEX) {
-            if (param.isMandatory()) {
-                param.setConstructor(!param.isConstructor());
-                fireTableCellUpdated(rowIndex, columnIndex);
-            }
         }
-    }
-
-    public void setParameters(List<BuilderParameter> params) {
-        parameters = params;
-    }
-
-    public List<BuilderParameter> getParameters() {
-        return parameters;
     }
 }
